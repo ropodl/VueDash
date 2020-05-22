a148a49965ab1acfaf08aa93b8c24a1306c57596
Saroj Poudel <sarox14@gmail.com>
2020-05-22 14:22:06 +0545

    back to analytics

--- src/plugins/analytics.js
+++ src/plugins/analytics.js
@@ -1,15 +1,15 @@
-// import Vue from 'vue'
-// import VueAnalytics from 'vue-analytics'
+import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 
 // import routes from '../router/'
 
-// Vue.use(VueAnalytics, {
-//     id: 'UA-167286605-1',
-//     checkDuplicatedScript: true,
-//     routes,
-//     autoTracking: {
-//         screenview: true
-//     }
-// })
+Vue.use(VueAnalytics, {
+    id: 'UA-167286605-1',
+    // checkDuplicatedScript: true,
+    // routes,
+    autoTracking: {
+        screenview: true
+    }
+})
 
-// export default VueAnalytics
+export default VueAnalytics

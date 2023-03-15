77fc06e68e1a50b28238afba6728cb58e666caeb
ropodl <sarox14@gmail.com>
2023-03-15 15:34:15 +0545

    removed apexcharts

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -3,9 +3,8 @@ import { loadFonts } from "./webfontloader";
 import vuetify from "./vuetify";
 import router from "../router";
 import i18n from "./i18n";
-import VueApexCharts from "vue3-apexcharts";
 
 export function registerPlugins(app) {
   loadFonts();
-  app.use(vuetify).use(router).use(i18n).use(VueApexCharts);
+  app.use(vuetify).use(router).use(i18n);
 }

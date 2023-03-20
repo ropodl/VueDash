c50b6f34127ff92be44fd11eee0ff7d2491603ad
Saroj Poudel <sarox14@gmail.com>
2023-03-21 00:43:29 +0545

    Update Chat.vue
    chat is beautiful

--- src/views/Pages/Chat.vue
+++ src/views/Pages/Chat.vue
@@ -1,17 +1,30 @@
 <script setup>
-import { mdiAttachment, mdiCallMade, mdiCircle } from "@mdi/js";
 import { ref } from "vue";
+import {
+  mdiImage,
+  mdiFileGifBox,
+  mdiEmoticonHappyOutline,
+  mdiDotsHorizontal,
+  mdiInformation,
+  mdiMessageVideo,
+  mdiPhone,
+  mdiPlusCircle,
+  mdiThumbUp,
+  mdiStickerEmoji,
+  mdiCircle,
+  mdiReply,
+  mdiMore,
+} from "@mdi/js";
 
 let tab = ref(0);
 let items = [
   {
     name: "Ashley Briggs",
-    image: "https://appstack.bootlab.io/img/avatars/avatar-5.jpg",
-    status: "Online",
+    image: "https://appstack.bootlab.io/img/avatars/avatar.jpg",
+    status: "online",
     msgCount: 5,
     message: [
       {
-        sender: "not-me",
         text: "Hello there I am text",
       },
       {
@@ -22,44 +35,58 @@ let items = [
   },
   {
     name: "Tim Cooks",
-    image: "https://appstack.bootlab.io/img/avatars/avatar-5.jpg",
-    status: "Online",
-    msgCount: 5,
+    image: "https://appstack.bootlab.io/img/avatars/avatar-2.jpg",
+    status: "Do not Disturb",
+    msgCount: 0,
   },
   {
     name: "John Doe",
-    image: "https://appstack.bootlab.io/img/avatars/avatar-5.jpg",
-    status: "Online",
-    msgCount: 5,
+    image: "https://appstack.bootlab.io/img/avatars/avatar-3.jpg",
+    status: "offline",
+    msgCount: 2,
   },
   {
     name: "Jimmy Doe",
-    image: "https://appstack.bootlab.io/img/avatars/avatar-5.jpg",
-    status: "Online",
-    msgCount: 5,
+    image: "https://appstack.bootlab.io/img/avatars/avatar-4.jpg",
+    status: "online",
+    msgCount: 0,
   },
   {
     name: "Fillip Nas",
     image: "https://appstack.bootlab.io/img/avatars/avatar-5.jpg",
-    status: "Online",
-    msgCount: 5,
+    status: "online",
+    msgCount: 0,
   },
 ];
 
 const getStatus = (status) => {
-  return status;
+  if (status == "online") {
+    return "green";
+  } else if (status == "Do not Disturb") {
+    return "error";
+  } else return "grey-darken-3";
+};
+const isMe = (sender) => {
+  return sender == "me" ? true : false;
+};
+const test = (i) => {
+  alert("test" + i);
 };
 </script>
 <template>
-  <v-container>
+  <v-container class="chat-page">
     <v-row>
       <v-col cols="12" class="py-0">
         <h1>Messages {{ tab }}</h1>
       </v-col>
       <v-col cols="12">
-        <v-row>
+        <v-row no-gutters>
           <v-col cols="12" md="4">
-            <v-card border flat>
+            <v-card
+              border
+              flat
+              class="h-100 border-e-0 rounded-te-0 rounded-be-0"
+            >
               <v-card-text>
                 <v-text-field
                   density="compact"
@@ -76,29 +103,67 @@ const getStatus = (status) => {
                   height="auto"
                   class="text-capitalize px-0"
                 >
-                  <v-list bg-color="transparent" style="border: 1px solid">
-                    <v-list-item
-                      :prepend-avatar="item['image']"
-                      :title="item['name']"
-                    >
-                      <v-list-item-subtitle>
-                        <v-icon
-                          start
-                          :color="item['status'] == 'Online' ? 'green' : 'red'"
-                          :icon="mdiCircle"
-                        ></v-icon>
-                        {{ item["status"] }}
-                      </v-list-item-subtitle>
-                    </v-list-item>
+                  <v-list
+                    density="compact"
+                    bg-color="transparent"
+                    class="w-100"
+                  >
+                    <v-hover v-slot="{ isHovering, props }">
+                      <v-list-item v-bind="props">
+                        <template #prepend>
+                          <v-avatar size="50">
+                            <v-img :src="item['image']"></v-img>
+                          </v-avatar>
+                        </template>
+                        <v-list-item-title class="d-flex">
+                          {{ item["name"] }}
+                        </v-list-item-title>
+                        <v-list-item-subtitle
+                          class="d-flex align-center text-capitalize"
+                        >
+                          <v-icon
+                            start
+                            :color="getStatus(item['status'])"
+                            :icon="mdiCircle"
+                          ></v-icon>
+                          {{ item["status"] }}
+                        </v-list-item-subtitle>
+                        <template #append>
+                          <template v-if="isHovering">
+                            <v-btn
+                              icon
+                              size="small"
+                              variant="tonal"
+                              class="mr-3"
+                              @click.stop="test(i)"
+                            >
+                              <v-icon :icon="mdiDotsHorizontal"></v-icon>
+                            </v-btn>
+                          </template>
+                          <template v-if="item['msgCount']">
+                            <v-chip
+                              variant="flat"
+                              density="compact"
+                              color="green"
+                              v-text="item['msgCount']"
+                            ></v-chip>
+                          </template>
+                        </template>
+                      </v-list-item>
+                    </v-hover>
                   </v-list>
                 </v-tab>
               </v-tabs>
             </v-card>
           </v-col>
           <v-col cols="12" md="8">
-            <v-card border flat>
-              <v-window v-model="tab" transition="none">
-                <v-window-item v-for="(item, i) in items" :value="i">
+            <v-card border flat class="rounded-ts-0 rounded-bs-0">
+              <v-window v-model="tab">
+                <v-window-item
+                  transition="none"
+                  v-for="(item, i) in items"
+                  :value="i"
+                >
                   <v-card flat>
                     <v-card-text class="d-flex align-center py-0 pl-0">
                       <v-list>
@@ -109,47 +174,88 @@ const getStatus = (status) => {
                         ></v-list-item>
                       </v-list>
                       <v-spacer> </v-spacer>
-                      <v-btn flat icon rounded="sm">
-                        <v-icon :icon="mdiCallMade"></v-icon>
+                      <v-btn flat icon variant="text">
+                        <v-icon :icon="mdiPhone"></v-icon>
+                      </v-btn>
+                      <v-btn flat icon variant="text">
+                        <v-icon :icon="mdiMessageVideo"></v-icon>
+                      </v-btn>
+                      <v-btn flat icon variant="text">
+                        <v-icon :icon="mdiInformation"></v-icon>
                       </v-btn>
                     </v-card-text>
                     <v-divider></v-divider>
                     <v-card-text class="overflow-auto" style="height: 500px">
-                      <div class="d-flex" v-for="(msg, i) in item['message']">
-                        <template v-if="msg['sender'] !== 'me'">
-                          <v-avatar class="mr-3">
-                            <v-img :src="item['image']"></v-img>
-                          </v-avatar>
-                        </template>
-                        <v-alert
-                          rounded="pill"
-                          color="primary"
-                          class="mb-3"
-                          :class="msg['sender'] == 'me' ? 'ml-auto' : 'mr-auto'"
-                          max-width="70%"
-                          density="compact"
-                          style="width: max-content !important"
-                        >
-                          {{ msg["text"] }}
-                        </v-alert>
-                      </div>
+                      <template v-for="msg in item['message']">
+                        <v-hover v-slot="{ isHovering, props }">
+                          <div
+                            v-bind="props"
+                            class="d-inline-flex align-center mb-3 justify-start w-100"
+                            :class="
+                              isMe(msg['sender']) ? 'flex-row-reverse' : ''
+                            "
+                          >
+                            <template v-if="msg['sender'] !== 'me'">
+                              <v-avatar class="mr-3">
+                                <v-img :src="item['image']"></v-img>
+                              </v-avatar>
+                            </template>
+                            <v-card
+                              flat
+                              rounded="pill"
+                              :color="
+                                isMe(msg['sender'])
+                                  ? 'primary'
+                                  : 'grey-darken-3'
+                              "
+                              density="compact"
+                            >
+                              <v-card-text>
+                                {{ msg["text"] }}
+                              </v-card-text>
+                            </v-card>
+                            <template v-if="isHovering">
+                              <v-btn icon size="small" variant="text">
+                                <v-icon
+                                  :icon="mdiEmoticonHappyOutline"
+                                ></v-icon>
+                              </v-btn>
+                              <v-btn icon size="small" variant="text">
+                                <v-icon :icon="mdiReply"></v-icon>
+                              </v-btn>
+                              <v-btn icon size="small" variant="text">
+                                <v-icon :icon="mdiDotsHorizontal"></v-icon>
+                              </v-btn>
+                            </template>
+                          </div>
+                        </v-hover>
+                      </template>
                     </v-card-text>
                     <v-divider></v-divider>
                     <v-card-text class="d-flex">
-                      <v-btn icon class="mr-3">
-                        <v-icon :icon="mdiAttachment"></v-icon>
+                      <v-btn icon variant="tonal" size="small" class="mr-1">
+                        <v-icon :icon="mdiPlusCircle"></v-icon>
+                      </v-btn>
+                      <v-btn icon variant="tonal" size="small" class="mr-1">
+                        <v-icon :icon="mdiImage"></v-icon>
+                      </v-btn>
+                      <v-btn icon variant="tonal" size="small" class="mr-1">
+                        <v-icon :icon="mdiStickerEmoji"></v-icon>
+                      </v-btn>
+                      <v-btn icon variant="tonal" size="small" class="mr-1">
+                        <v-icon :icon="mdiFileGifBox"></v-icon>
                       </v-btn>
                       <v-text-field
-                        class="mx-3"
-                        density="comfortable"
                         hide-details
+                        color="white"
+                        class="mx-3 border-0"
+                        density="compact"
                         placeholder="Say hi"
+                        :append-inner-icon="mdiEmoticonHappyOutline"
+                        @click:append-inner="test"
                       ></v-text-field>
-                      <v-btn icon class="mx-3">
-                        <v-icon :icon="mdiAttachment"></v-icon>
-                      </v-btn>
-                      <v-btn icon>
-                        <v-icon :icon="mdiAttachment"></v-icon>
+                      <v-btn icon variant="tonal" size="small" class="mr-1">
+                        <v-icon :icon="mdiThumbUp"></v-icon>
                       </v-btn>
                     </v-card-text>
                   </v-card>
@@ -162,42 +268,8 @@ const getStatus = (status) => {
     </v-row>
   </v-container>
 </template>
-<script>
-export default {
-  name: "Comments",
-  // props: ["commentData", "msgError"],
-  data() {
-    return {
-      commentData: [],
-      msgError: "test",
-      comment: "",
-      time: "",
-      error: this.msgError,
-    };
-  },
-  watch: {
-    commentData: function () {
-      this.$nextTick(function () {
-        this.scrollToEnd();
-      });
-    },
-  },
-  mounted() {
-    // this.scrollToEnd();
-  },
-  methods: {
-    scrollToEnd() {
-      this.$vuetify.goTo(this.commentData.length != 0 ? "#last" : "#nothing", {
-        container: "#scroll-target",
-      });
-    },
-    sendData() {
-      if (this.comment) {
-        this.$emit("getResponse", this.comment);
-        this.comment = "";
-        this.error = this.msgError;
-      } else return 0;
-    },
-  },
-};
-</script>
+<style lang="scss">
+.chat-page .v-tab .v-btn__content {
+  width: 100% !important;
+}
+</style>

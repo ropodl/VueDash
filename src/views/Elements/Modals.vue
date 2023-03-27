e6ed936b86322b848eb60483e2aae7169be6543c
ropodl <sarox14@gmail.com>
2023-03-28 04:27:01 +0545

    Update Modals.vue

--- src/views/Elements/Modals.vue
+++ src/views/Elements/Modals.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { ref, reactive } from "vue";
 import { useTitle } from "@vueuse/core";
+import { mdiClose, mdiFuse } from "@mdi/js";
 useTitle("Modals Example Page");
 
 let normal = reactive({});
 let colored = reactive({});
-let centered = reactive({});
 let size = reactive({});
+let full = ref(false);
 
 let colors = [
   {
@@ -267,6 +268,73 @@ let colors = [
                     </v-btn>
                   </v-col>
                 </v-row>
+              </v-card>
+            </v-dialog>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col col="12" md="6">
+        <v-card border flat title="Fullscreen Modal">
+          <v-card-text>
+            <v-dialog
+              v-model="full"
+              fullscreen
+              :scrim="false"
+              transition="dialog-bottom-transition"
+            >
+              <template v-slot:activator="{ props }">
+                <v-btn color="primary" v-bind="props"> Open Dialog </v-btn>
+              </template>
+              <v-card rounded="0">
+                <v-toolbar color="primary">
+                  <v-btn icon @click="full = false">
+                    <v-icon :icon="mdiFuse"></v-icon>
+                  </v-btn>
+                  <v-toolbar-title>Settings</v-toolbar-title>
+                  <v-spacer></v-spacer>
+                  <v-toolbar-items>
+                    <v-btn variant="text" @click="full = false">Save</v-btn>
+                  </v-toolbar-items>
+                </v-toolbar>
+                <v-list lines="two" subheader>
+                  <v-list-subheader>User Controls</v-list-subheader>
+                  <v-list-item
+                    title="Content filtering"
+                    subtitle="Set the content filtering level to restrict apps that can be downloaded"
+                  ></v-list-item>
+                  <v-list-item
+                    title="Password"
+                    subtitle="Require password for purchase or use password to restrict purchase"
+                  ></v-list-item>
+                </v-list>
+                <v-divider></v-divider>
+                <v-list lines="two" subheader>
+                  <v-list-subheader>General</v-list-subheader>
+                  <v-list-item
+                    title="Notifications"
+                    subtitle="Notify me about updates to apps or games that I downloaded"
+                  >
+                    <template v-slot:prepend>
+                      <v-checkbox :model-value="false"></v-checkbox>
+                    </template>
+                  </v-list-item>
+                  <v-list-item
+                    title="Sound"
+                    subtitle="Auto-update apps at any time. Data charges may apply"
+                  >
+                    <template v-slot:prepend>
+                      <v-checkbox :model-value="true"></v-checkbox>
+                    </template>
+                  </v-list-item>
+                  <v-list-item
+                    title="Auto-add widgets"
+                    subtitle="Automatically add home screen widgets"
+                  >
+                    <template v-slot:prepend>
+                      <v-checkbox :model-value="true"></v-checkbox>
+                    </template>
+                  </v-list-item>
+                </v-list>
               </v-card>
             </v-dialog>
           </v-card-text>
